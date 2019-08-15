39074317cbba2b8b48c139b1920c6f7efbca1dc9
Oliver <muriithioliver11@outlook.com>
2019-08-15 15:27:38 +0300

    This will add required routing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,9 +14,30 @@ public class App {
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
+        post("/profile/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            response.redirect("/signIn");
+            return null;
+        }, new HandlebarsTemplateEngine());
+
+        get("/signIn", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "signIn.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/challenges", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "challenge.hbs");
+        }, new HandlebarsTemplateEngine());
+
         get("/challenges", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "challenge.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/view", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "view.hbs");
         }, new HandlebarsTemplateEngine());
 
         get("/challenge", (request, response) -> {
